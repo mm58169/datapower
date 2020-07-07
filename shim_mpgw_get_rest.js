68f9fc847c65647f1815fcf4efa672105b3fb512
Robert Peterson <robertpeterson2@massmutual.com>
2020-07-07 11:53:09 -0400

    dynamic JSON backend functional

--- shim_mpgw_get_rest.js
+++ shim_mpgw_get_rest.js
@@ -1,3 +1,23 @@
-console.info("Starting REST to JSON route");
 
-console.info(`content of contentID Var: ${sm.getVar('context/POC/contentID')}`);
+const urlmodule = require('urlopen');
+
+let ctx = session.name('POC');
+let contentID = ctx.getVariable('contentID');
+
+const url = `http://vdicorcata106.na.mmfg.net:8080/getContentByID?contentid=${contentID}`;
+
+console.info('####', Object.getOwnPropertyNames(contentID));
+
+
+session.output.write(contentID);
+
+/*
+urlmodule.open({
+  target: url,
+  method: 'get'
+}, (error, response) => {
+  response.readAsBuffer((error, responseData) => {
+    session.output.write(responseData);
+  });
+});
+*/
